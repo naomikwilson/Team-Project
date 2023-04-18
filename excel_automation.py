0453686f4a9a61c85fdbc6895aeaddac96c076f4
MSERG01 <savilabermudez1@babson.edu>
2023-04-17 22:55:09 -0400

    update

--- excel_automation.py
+++ excel_automation.py
@@ -1,5 +1,7 @@
 import os
 import win32com.client as client
+from openpyxl import load_workbook, Workbook
+from openpyxl.utils import column_index_from_string
 
 
 def convert_xls_to_xlsm(folder_path):
@@ -13,11 +15,29 @@ def convert_xls_to_xlsm(folder_path):
     excel.Quit()
 
 
+def add_sheet_and_vba(filepath):
+    # open the existing workbook
+    wb = load_workbook(filepath)
+    analysis_sheet = wb.create_sheet("Compco and Benchmarking Analysis")
+
+    wb.save(filepath)
+
+
 def main():
     user = "savilabermudez1"
-    folder_path = f"C:/Users/{user}/Documents/GitHub/Team-Project/excel_files/"
-    print(folder_path)
-    convert_xls_to_xlsm(folder_path)
+    folder_path = f"C://Users/{user}//Documents//GitHub//Team-Project//excel_files//"
+    file_name = "Company Comparable Analysis Apple Inc .xlsm"
+    sample_file_path = os.path.join(folder_path, file_name)
+    print(sample_file_path)
+
+    add_sheet_and_vba(sample_file_path)
+
+    text_code = '''
+    Sub HelloWorld()
+        MsgBox "Hello, World
+    End Sub    
+
+    '''
 
 
 if __name__ == '__main__':
