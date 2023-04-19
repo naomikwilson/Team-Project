2453658dbc37cb0af86dd5f6c4a690d8ae596605
MSERG01 <savilabermudez1@babson.edu>
2023-04-18 20:05:52 -0400

    mass convert files

--- excel_automation.py
+++ excel_automation.py
@@ -1,49 +1,59 @@
 import os
 import xlrd
 from openpyxl import load_workbook, Workbook
+import aspose.cells
+import xlwings as xw
 
 
-def convert_xls_to_xlsm(folder_path):
+def xls_to_xlsm(file_path):
+    """"
+    https://products.aspose.com/cells/python-net/conversion/xls-to-xlsm/
+    """
+    workbook = aspose.cells.Workbook(file_path)
+    # Split the file path into its directory and file name components
+    directory, filename = os.path.split(file_path)
+
+    # # Change the file extension from ".xls" to ".xlsm"
+    new_filename = os.path.splitext(filename)[0] + ".xlsm"
+
+    # # Combine the directory and new filename components into a new file path
+    new_folder_path = os.path.join(directory, new_filename)
+    new_folder_path = new_folder_path.replace("\\", "/")
+    workbook.save(new_folder_path)
+    os.remove(file_path)
+
+
+def mass_convert_xls_to_xlsm(folder_path):
     for file_name in os.listdir(folder_path):
         if file_name.endswith(".xls"):
-            # open xls file using xlrd
-            xls_book = xlrd.open_workbook(os.path.join(folder_path, file_name))
-
-            # create new XLSM book using openpyxl
-            xlsm_new_book = Workbook()
-
-            for sheet_name in xls_book.sheet_name():
-                sheet = xls_book.sheet_by_name(sheet_name)
-                new_sheet = xlsm_new_book.create_sheet(title=sheet_name)
-                for row_index in range(sheet.nrows()):
-                    new_sheet.append(sheet.row_value(row_index))
-        name, _ = os.path.splitext(file_name)
-        xlsm_new_book.save(os.path.join(folder_path, name + "xlsm"))
+            file_path = folder_path + "/" + file_name
+            xls_to_xlsm(file_path)
 
 
-def add_sheet_and_vba(filepath):
-    pass
+def add_sheet(file_path):
+    """
+
+    """
+    print(file_path)
+    print("-"*50)
+    wb = load_workbook(file_path)
+    # create sheet for analysis
+    analysis_sheet = wb.create_sheet("Compco and Benchmarking")
+    # save excel
+    wb.save(file_path)
 
 
 def main():
-    user = "savilabermudez1"
-    folder_path = f"C://Users/{user}//Documents//GitHub//Team-Project//excel_files//"
-    file_name = "Company Comparable Analysis Apple Inc .xlsm"
 
-    sample_file_path = os.path.join(folder_path, file_name)
-    print(sample_file_path)
+    user = input("Enter your Babson Username -> ")
+    folder_path = f"C:/Users/{user}/Documents/GitHub/Team-Project/excel_files"
+    mass_convert_xls_to_xlsm(folder_path)
 
-    convert_xls_to_xlsm(folder_path)
 
-    # add_sheet_and_vba(sample_file_path)
-
+    # add_sheet_all(folder_path)
     # text_code = '''
     # Sub HelloWorld()
     #     MsgBox "Hello, World
     # End Sub
-
-    # '''
-
-
 if __name__ == '__main__':
     main()
