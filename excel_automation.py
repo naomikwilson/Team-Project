7450e5ee0261270cd768967a4deffc2493585fbe
MSERG01 <savilabermudez1@babson.edu>
2023-04-19 20:58:18 -0400

    Updated Excel Code

--- excel_automation.py
+++ excel_automation.py
@@ -1,13 +1,16 @@
 import os
-import xlrd
+import pandas as pd
 from openpyxl import load_workbook, Workbook
 import aspose.cells
 import xlwings as xw
+from vba_modules import vba_code
 
 
 def xls_to_xlsm(file_path):
     """"
     https://products.aspose.com/cells/python-net/conversion/xls-to-xlsm/
+
+    Converts XLS to XLSM - XLS is old excel and not compatible with Openpyxl
     """
     workbook = aspose.cells.Workbook(file_path)
     # Split the file path into its directory and file name components
@@ -24,50 +27,77 @@ def xls_to_xlsm(file_path):
 
 
 def mass_convert_xls_to_xlsm(folder_path):
+    """
+    Loops through all XLS files and converts them to XLSM (macro enabled workbook)
+
+    """
+
     for file_name in os.listdir(folder_path):
         if file_name.endswith(".xls"):
             file_path = folder_path + "/" + file_name
             xls_to_xlsm(file_path)
 
 
-def inject_vba_code(file_path):
+def mass_analysis(folder_path):
+
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".xlsm"):
+            file_path = folder_path + "/" + file_name
+            inject_vba_code(file_path, vba_code)
+
+
+def extract_macro_names(vba_code):
     """
+    Extracts macro name from str which is used to execute code
+    """
+    macro_names = []
+    for line in vba_code.splitlines():
+        if line.startswith("Sub "):
+            macro_names.append(line[4:].strip("()"))
+    return macro_names
+
+
+def inject_vba_code(file_path, vba_code):
+    """
+    Adds new sheet for analysis and injects vba macro to preform analysis.
 
     """
     # open the workbook in excel
     wb = xw.Book(file_path)
+    analysis_sheet_name = "COMPCO AND BENCHMARKING"
+    add_before = "Financial Data"
+    # add analysis sheet
+    try:
+        wb.sheets.add(analysis_sheet_name, before=add_before)
+    except ValueError:
+        pass
+    # open VBA editor
+    vb = wb.app.api.VBE
+    # add new module
+    analysis_module = vb.VBProjects(1).VBComponents.Add(1)
+    analysis_module.CodeModule.AddFromString(vba_code)
+    # Execute all macros in module
+    module_name = "Module1."
+    macro_names = extract_macro_names(vba_code)
+    print(macro_names)
+    for macro in macro_names:
+        macro = wb.macro(module_name + macro)
+        macro()
 
-    # open the VBA editor
-    wb.app.api.VBE.MainWindow.Visible = True
-
-    # get a reference to the VBA Project
-    vba_project = wb.app.api.VBE.ActiveVBProject
-
-    # Create New Module and inject code
-    module = vba_project.VBComponents.Add(1)
-    module.CodeModule.AddFromString("My Function() \n ")
-
-    # save changes and close the VBA editor
-    wb.app.api.VBE.MainWindow.Visible = False
-    wb.save()
+    # close and save workbook
+    # wb.save()
+    # wb.close()
 
 
 def main():
-
-    user = input("Enter your Babson Username -> ")
-    folder_path = f"C:/Users/{user}/Documents/GitHub/Team-Project/excel_files"
-    mass_convert_xls_to_xlsm(folder_path)
-
+    # user = input("Enter your Babson Username -> ")
+    # folder_path = f"C:/Users/{user}/Documents/GitHub/Team-Project/excel_files"
+    # mass_convert_xls_to_xlsm(folder_path)
     file_path = "C:/Users/savilabermudez1/Documents/GitHub/Team-Project/excel_files/Company Comparable Analysis Apple Inc  (1).xlsm"
     print(file_path)
-    inject_vba_code(file_path)
-    print("Done")
+    inject_vba_code(file_path, vba_code)
+    print(f"-"*50, "\nDone")
 
 
-    # add_sheet_all(folder_path)
-    # text_code = '''
-    # Sub HelloWorld()
-    #     MsgBox "Hello, World
-    # End Sub
 if __name__ == '__main__':
     main()
